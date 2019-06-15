2c3db46c475698e36e351a3a85bf7ff429eb2546
Tabassum <tabs22sultana@gmail.com>
2019-06-15 12:12:27 +0530

    First

---
+++ Ashwin/src/main/java/com/example/Ashwin/AshwinApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ashwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshwinApplication.class, args);
+	}
+
+}
